a14813fb27bdfd8861a7c13a93e4efe11aa0eb52
董文涵 <dongwenhan@wenhan-mbp.home>
2025-06-24 13:27:59 -0400

    Update api link

--- src/App.vue
+++ src/App.vue
@@ -125,7 +125,7 @@ export default {
         obj.sdImage = "";
         console.log("hello before response")
         // Call the Flask API to generate an image
-        const response = await fetch('http://127.0.0.1:5001/api/generate-image', {
+        const response = await fetch('https://moodjournal-2.onrender.com', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
